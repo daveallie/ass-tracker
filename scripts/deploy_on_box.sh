5e67ea9def946a3c84ad553f8a368cb1c666d7f4
Dave Allie <dave@daveallie.com>
2019-10-26 02:02:13 +1000

    Add some logs to deployment script

--- scripts/deploy_on_box.sh
+++ scripts/deploy_on_box.sh
@@ -9,18 +9,27 @@ fi
 
 deploymentRoot="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
+echo "\n\n  ===  Navigating to $deploymentRoot/backend  ==="
 cd $deploymentRoot/backend
+echo "\n\n  ===  Installing backend dependencies  ==="
 yarn
 
+echo "\n\n  ===  Navigating to $deploymentRoot/frontend  ==="
 cd $deploymentRoot/frontend
+echo "\n\n  ===  Installing frontned dependencies  ==="
 yarn
+echo "\n\n  ===  Building production ready frontend assets  ==="
 NODE_ENV=production yarn build
 
+echo "\n\n  ===  Stopping running backend  ==="
 pm2 stop backend || true
+echo "\n\n  ===  Mapping current deployment to $deploymentRoot  ==="
 rm /home/ubuntu/server/current || true
 ln -s $deploymentRoot /home/ubuntu/server/current
+echo "\n\n  ===  Starting backend  ==="
 NODE_ENV=production pm2 start --name backend /home/ubuntu/server/current/backend/bin/www
 
+echo "\n\n  ===  Cleaning up old deployments  ==="
 # Remove all but 10 latest deployments
 cd $deploymentRoot/..
 ls -1 | head -n -10 | xargs -d '\n' rm -rf --
--- scripts/start_deploy.sh
+++ scripts/start_deploy.sh
@@ -13,6 +13,6 @@ deployPath=/home/ubuntu/server/deploys
 ssh ubuntu@3.104.76.139 << EOF
   cd $deployPath
   git clone --depth 1 https://github.com/daveallie/ass-tracker.git $deployStamp
-  # cd $deployStamp/scripts
-  # ./deploy_on_box.sh
+  cd $deployStamp/scripts
+  ./deploy_on_box.sh
 EOF
