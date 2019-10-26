4a5b9d40919d3b26bd8c2432dfc54abe43c0e4af
James Baxter <j.w.baxter@gmail.com>
2019-10-26 11:46:09 +1000

    Enrich assets with last user

--- backend/tanda/client.js
+++ backend/tanda/client.js
@@ -1,23 +1,29 @@
 const axios = require("axios").default;
-const { uniq } = require("lodash");
-const { getTandaApiToken } = require("./auth");
+const _ = require("lodash");
+const {getTandaApiToken} = require("./auth");
 
-const getAllUsers = req => {
-  axios.get("https://my.tanda.co/api/v2/users", {
-    headers: { Authorization: `bearer ${getTandaApiToken(req) }` },
-  })
-    .then(response => response.data)
-};
+const getAllUsers = req =>
+    axios.get("https://my.tanda.co/api/v2/users", {
+      headers: {Authorization: `bearer ${getTandaApiToken(req)}`},
+    }).then(response => response.data);
 
-const getAssetList = req =>
-  axios.get("https://my.tanda.co/api/v2/platform/car", {
-    headers: { Authorization: `bearer ${getTandaApiToken(req) }` },
-  })
-    .then(response => response.data)
-    .then(data => {
-      // const userIdsToPull = uniq(data.map(asset => asset.lastUserId).map(id => id.toString()).filter(id => id));
-      return data
-    });
+const getAssets = req =>
+    axios.get("https://my.tanda.co/api/v2/platform/car", {
+      headers: {Authorization: `bearer ${getTandaApiToken(req)}`},
+    }).then(response => response.data);
+
+const getAssetList = req => Promise.all([getAssets(req), getAllUsers(req)]).then(([assets, allUsers]) => {
+
+  const userIdsToPull = _.uniq(assets.map(asset => asset.last_user_id).filter(id => id != null));
+
+  const relevantUsers = _.filter(allUsers, user => _.includes(userIdsToPull, user.id));
+
+  const userMap = _.keyBy(relevantUsers, user => user.id);
+
+  _.forEach(assets, asset => asset['user'] = userMap[asset.last_user_id]);
+
+  return assets;
+});
 
 module.exports = {
   getAssetList
