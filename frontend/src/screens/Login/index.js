ff8dcca0ea88ad913c2926b7ed6b9141aaa72340
Dave Allie <dave@daveallie.com>
2019-10-26 15:27:06 +1000

    Loading spinner change

--- frontend/src/screens/Login/index.js
+++ frontend/src/screens/Login/index.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import {Button, Input} from "semantic-ui-react";
 import { login } from "../../util/api";
 import styles from "./styles.module.css";
 
-const handleLogin = () => login()
-  .then(() => window.location.reload());
+const Login = () => {
+  const [loading, setLoading] = useState(false);
 
-const Login = () => (
-  <div className={styles.container}>
-    <img className={styles.icon} src="https://cdn2.iconfinder.com/data/icons/fashion-beauty-accessories/128/underwear-thong-butt-512.png" />
-    <div className={styles.loginBox}>
-      <div className={styles.loginText}>Login</div>
-      <div className={styles.inputs}>
-        <Input fluid placeholder='Username' />
-        <Input fluid placeholder='Password' />
+  const handleLogin = () => {
+    setLoading(true);
+    login()
+      .then(() => window.location.reload())
+  };
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.icon} src="https://cdn2.iconfinder.com/data/icons/fashion-beauty-accessories/128/underwear-thong-butt-512.png" />
+      <div className={styles.loginBox}>
+        <div className={styles.loginText}>Login</div>
+        <div className={styles.inputs}>
+          <Input fluid placeholder='Username' />
+          <Input fluid placeholder='Password' />
+        </div>
+        <Button secondary loading={loading} onClick={handleLogin}>Login</Button>
       </div>
-      <Button secondary onClick={handleLogin}>Login</Button>
     </div>
-  </div>
-);
+  );
+}
 
 export default Login;
