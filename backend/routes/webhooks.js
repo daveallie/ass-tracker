a0ba90f46de8482218cb14ef86dc0f51ef5fc4a1
James Baxter <j.w.baxter@gmail.com>
2019-10-27 20:20:16 +1000

    Cleanup webhook

--- backend/routes/webhooks.js
+++ backend/routes/webhooks.js
@@ -4,37 +4,25 @@ const {triggerClientsUpdate} = require("../util/websockets");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  console.log("Got data!", req.body);
+const processShiftStart = (req, shift_id, user_id) => {
+  getShift(req, shift_id).then(shift => {
+    console.log("Got shift", shift);
+    const asset_id = shift.tag;
+    const asset = {
+      car_in_use: true,
+      last_user_id: user_id
+    };
+    updateAsset(req, asset_id, asset);
+  })
+};
 
-  const payload = req.body.payload;
-
-  if (payload.topic === 'platform_record.updated') {
-    triggerClientsUpdate()
-
-  } else if (payload.topic === "clockin.updated") {
-    const shift_id = payload.body.shift_id;
-    const shift_type = payload.body.type;
-    const user_id = payload.body.user_id;
-
-    if (shift_type === 'start') {
-      getShift(req, shift_id).then(shift => {
-        console.log("Got shift", shift);
-        const asset_id = shift.tag;
-        const asset = {
-          car_in_use: true,
-          last_user_id: user_id
-        };
-        return updateAsset(req, asset_id, asset);
-      })
-
-    } else if (shift_type === 'finish') {
-
-      getShift(req, shift_id).then(shift => {
+const processShiftEnd = (req, shift_id) => {
+  getShift(req, shift_id)
+      .then(shift => {
         console.log("Got shift", shift);
         const asset_id = shift.tag;
         if (asset_id === null)
-          throw Error("No shift ID!!");
+          throw Error("No asset ID!!");
         const shiftTimeSecs = shift.finish - shift.start;
         const shiftTimeHr = shiftTimeSecs === 0 ? 0 : shiftTimeSecs / 60 / 60;
         return {
@@ -42,7 +30,8 @@ router.post('/', (req, res) => {
           assetId: asset_id,
           shiftTime: shiftTimeHr
         };
-      }).then(shiftData =>
+      })
+      .then(shiftData =>
           getAsset(req, shiftData.assetId).then(asset => {
             console.log("Got asset", asset);
             const assetData = {
@@ -52,20 +41,37 @@ router.post('/', (req, res) => {
               total_hours_of_use: asset.total_hours_of_use + shiftData.shiftTime
             };
             return updateAsset(req, shiftData.assetId, assetData);
-          })).catch(e => console.log(e));
+          }))
+      .catch(e => console.log(e));
+};
 
-      // getShift(req, shift_id).then(shift => {
-      //   const asset_id = shift.tag;
-      //   const shiftTimeMin = shift.finish - shift.start;
-      //   const shiftTimeHr = shiftTimeMin === 0 ? 0 : shiftTimeMin / 60;
-      //   const asset = {
-      //     car_in_use: false,
-      //     hours_since_last_service: +shiftTimeHr,
-      //     total_hours_of_use: +shiftTimeHr
-      //   };
-      //   updateAsset(req, asset_id, asset);
-      // });
-    }
+const processCheckinUpdate = (req, body) => {
+  const shift_id = body.shift_id;
+  const user_id = body.user_id;
+
+  switch (body.type) {
+    case "start":
+      processShiftStart(req, shift_id, user_id);
+      break;
+    case "finish":
+      processShiftEnd(req, shift_id);
+      break;
+  }
+};
+
+router.post('/', (req, res) => {
+  console.log("Got webhook", req.body);
+
+  const payload = req.body.payload;
+  const topic = payload.topic;
+
+  switch (topic) {
+    case "platform_record.updated":
+      triggerClientsUpdate();
+      break;
+    case "clockin.updated":
+      processCheckinUpdate(req, payload.body);
+      break;
   }
   res.end();
 });
